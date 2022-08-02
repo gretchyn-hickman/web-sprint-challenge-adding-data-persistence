7c8d0e5969082c0239a06dd1b3be60b9237b2663
Gretchyn Hickman <82916149+gretchyn-hickman@users.noreply.github.com>
2022-08-02 10:24:57 -0600

    resource

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,19 @@
 // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+
+const resMod = require('./model')
+
+router.get('/', (req, res, next) => {
+    resMod.getResources()
+        .then(resAll => res.json(resAll))
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    resMod.create(req.body)
+        .then(obj => res.json(obj))
+        .catch(next);
+})
+
+module.exports = router;
